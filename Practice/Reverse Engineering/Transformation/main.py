afb569b73969ebab252adb94adfcd6ac5ba439a3
Amir <amirbrahamm@gmail.com>
2021-08-04 16:25:56 +0100

    stonks

--- Practice/Reverse Engineering/Transformation/main.py
+++ Practice/Reverse Engineering/Transformation/main.py
@@ -1,3 +1,11 @@
 flag = "灩捯䍔䙻ㄶ形楴獟楮獴㌴摟潦弸弰㑣〷㘰摽"
 
 # ''.join([chr((ord(flag[i]) << 8) + ord(flag[i + 1])) for i in range(0, len(flag), 2)])
+
+
+print(ord(flag[0]))
+
+DecryptedFlag = ""
+for i in range(len(flag)):
+    DecryptedFlag += chr(ord(flag[i]) >> 8)
+print(DecryptedFlag)
